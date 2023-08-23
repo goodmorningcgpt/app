6fbbae4f0e7d7ab020a8b18c4b9c89abfb4eae87
Kyle Brandt <knbrandt47@gmail.com>
2023-08-23 13:12:29 -0400

    initializing project code into git remote repo

--- server/index.js
+++ server/index.js
@@ -12,10 +12,6 @@ const queries = require('./sqlcontroller.js');
 
 dotenv.config();
 
-// Twilio account credentials
-const accountSid = "";
-const authToken = "";
-const client = twilio(accountSid, authToken);
 //SQL credentials
 const SQL_URI = process.env.SQL_URI;
 
@@ -23,12 +19,17 @@ const pool = new Pool({
   connectionString: SQL_URI,
 });
 
+// Twilio account credentials
+const accountSid = "AC5e57136a0b63339682fb1c5bdb550652";
+const authToken = "9676afa6e0b84c3bceec9dae5bc874b9";
+const client = twilio(accountSid, authToken);
+
 //open ai configuration
 const { Configuration, OpenAIApi } = require("openai");
 
 const configuration = new Configuration({
   organization: "org-dmrFp1Qvb5jsxzyHLMGXyHQX",
-  apiKey: "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",
+  apiKey: "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",
 });
 
 const openai = new OpenAIApi(configuration);
@@ -39,7 +40,7 @@ async function generateGPTResponse(message) {
     messages: [
       { 
         role: "system", 
-        content: "You are an assisant for our sms-based application. You will be talking to our users over sms. Our application's purpose is to send motivating text messages at 7:00am for users. Please keep the responses under 100 characters. If you get a message that you don't understand, tell the user that they can begin receiving messages from our service by texting 'morning' or stop receiving messages by texting 'pause'"
+        content: "You are an assisant for our sms-based application. You will be talking to our users over sms. Our application's purpose is to send motivating text messages at 7:00am for users. Please keep the responses under 100 characters. If you get a message that you don't understand, tell the user that they can begin receiving messages from our service by texting 'morning' or stop receiving messages by texting 'pause'. When you first greet users, tell them that they can start receiving motivational messages by texting 'morning'."
       },
       { 
         role: "user", 
@@ -73,31 +74,17 @@ async function handleResponse(number, req, res, message) {
     });
 }
 
-// test
-// handleResponse();
-
 
 // Parse incoming request body as text
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // Endpoint to handle incoming SMS messages
-app.post("/sms", (req, res) => {
+app.post("/sms", async (req, res) => {
   const { Body, From } = req.body;
+  
   const isUser = false;
-  // const await isUser = userExists();
+  // isUser = await queries.selectUser(From)
 
-  // // Test
-  // const message = `Hey, this works! You sent:'${Body}' from ${From}`;
-  // client.messages.create({
-  //   body: message,
-  //   from: '+16319003876',
-  //   to: From,
-  // }).then(() => {
-  //   res.send('Message sent!');
-  // }).catch((error) => {
-  //   console.error(error);
-  //   res.status(500).send('Error sending message');
-  // });
 
   if (Body.slice(0, 8).toLowerCase() === "morning") {
     //sql query to create user
@@ -113,7 +100,6 @@ app.post("/sms", (req, res) => {
     handleResponse(From, req, res, Body);
     //sql query to send options to a user
     // sendOptions(From, req, res);
-    // console.log("received message");
   }
 });
 
@@ -138,7 +124,8 @@ async function alreadySignedUp(number, req, res) {
 async function signupUser(number, req, res) {
   const response = `Awesome! You'll now receive morning messages from me. As a reminder, if you ever wish to unsubscribe, just send "pause".`;
 
-  //sql query to add a user
+  queries.addUser(number);
+
   client.messages
     .create({
       body: response,
@@ -163,6 +150,8 @@ async function removeUser(number, req, res) {
   const response = `You're now unsubscribed. If you ever change your mind, just send "morning", and I'll resume sending morning messages. Goodbye!`;
 
   //sql query to remove a user
+  queries.deleteUser(number);
+  
   client.messages
     .create({
       body: response,
@@ -199,36 +188,51 @@ function sendOptions(number, req, res) {
     });
 }
 
-// //Schedule task to run every hour
-// timezonelist.forEach(({ timezone, task }) => {
-//   cron.schedule("0 * * * *", () => {
-//     const now = moment().tz(timezone);
-//     if (now.hour() === 7) {
-//       sendMessages(timezone);
-//     }
-//   });
+// Schedule task to run every hour
+timezonelist.forEach(({ timezone, task }) => {
+  cron.schedule("0 * * * *", () => {
+    const now = moment().tz(timezone);
+    if (now.hour() === 7) {
+      sendMessages(timezone);
+    }
+  });
+});
+
+
+/* Testing */
+// cron.schedule("* * * * *", () => {
+//     sendMessages();
 // });
 
-// function sendMessages(timezone) {
-//   //query all users phone numbers
+async function runScheduledTask() {
+  await cron.schedule('* * * * *', () => {
+    sendMessages();
+  });
+  console.log('Scheduled task completed');
+}
 
-//   //create a list of users with their phone numbers and message to send them
-//   //execute texts
-// }
+runScheduledTask().catch(error => console.log(error));
 
-// // test endpoint for SQL
-// app.post('/sql', (req, res) => {
-//   pool
-//     .query('SELECT * FROM users')
-//     .then(res => {
-//       console.log('response: ', res);
-//       res.status(200).send();
-//     })
-//     .catch(err => {
-//       console.log('err: ', err);
-//       res.status(500).send();
-//   });
-// })
+async function sendMessages() {
+  //get the random message from the database
+  const messageOfDay = await queries.selectRandomMessage();
+  console.log(messageOfDay);
+  //query all users phone numbers
+  const numbers = await queries.getAllUsers();
+  //send all users the message of the day
+
+  numbers.rows.forEach((number) => {
+    console.log(number);
+    client.messages
+      .create({
+        body: messageOfDay,
+        from: "+16319003876",
+        to: number.phone_number
+      })
+      .then(message => console.log(message.sid))
+      .catch(error => console.log(error));
+  });
+}
 
 // Start the server and ngrok tunnel
 (async () => {
--- server/sqlcontroller.js
+++ server/sqlcontroller.js
@@ -1,55 +1,103 @@
 // SQL QUERIES
+const express = require("express");
+const bodyParser = require("body-parser");
+const twilio = require("twilio");
+const ngrok = require("ngrok");
+const timezonelist = require("./timezonelist.js");
+const { Pool } = require("pg");
+const dotenv = require("dotenv");
+const app = express();
+const port = 3000;
+const cron = require('node-cron');
+const queries = require('./sqlcontroller.js');
+
+dotenv.config();
+
+//SQL credentials
+const SQL_URI = process.env.SQL_URI;
+
+const pool = new Pool({
+  connectionString: SQL_URI,
+});
 
 module.exports = {
-  selectUser: (phoneNumber) => {
+  selectUser: async (phoneNumber) => {
     const text = 'SELECT * FROM users WHERE phone_number = $1';
-    pool.query(text, [phoneNumber])
-    .then(results => {
-      if (results.rows.length > 0) isUser = true;
-    })
-    .catch(err => console.log(err));
+    try {
+      const results = await pool.query(text, [phoneNumber]);
+      return results.rows.length > 0;
+    } catch (err) {
+      console.log(err);
+    }
   },
-
-  deleteUser: (phoneNumber) => {
+  
+  deleteUser: async (phoneNumber) => {
     const text = 'DELETE FROM users WHERE phone_number = $1';
-    pool.query(text, [phoneNumber])
-      .then(results => console.log(results))
-      .catch(err => console.log(err));
+    try {
+      const results = await pool.query(text, [phoneNumber]);
+      console.log(results);
+    } catch (err) {
+      console.log(err);
+    }
   },
-
-  addUser: (phoneNumber) => {
+  
+  addUser: async (phoneNumber) => {
     const text = 'INSERT INTO users (phone_number, time_zone) VALUES ($1, $2)';
-    pool.query(text, [phoneNumber, 'America/New_York'])
-    .then(results => console.log(results))
-    .catch(err => console.log(err));
+    try {
+      const results = await pool.query(text, [phoneNumber, 'America/New_York']);
+      console.log(results);
+    } catch (err) {
+      console.log(err);
+    }
   },
-
-  deleteAllUsers: () => {
+  
+  getAllUsers: async () => {
+    const text = 'SELECT phone_number FROM users';
+    try {
+      const results = await pool.query(text);
+      return results;
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  
+  deleteAllUsers: async () => {
     const text = 'DELETE FROM users';
-    pool.query(text)
-      .then(results => console.log(results))
-      .catch(err => console.log(err));
+    try {
+      const results = await pool.query(text);
+      console.log(results);
+    } catch (err) {
+      console.log(err);
+    }
   },
-
-  addMessage: (message) => {
+  
+  addMessage: async (message) => {
     const text = 'INSERT INTO messages (message) VALUES ($1)';
-    pool.query(text, [message])
-      .then(results => console.log(results))
-      .catch(err => console.log(err));
+    try {
+      const results = await pool.query(text, [message]);
+      console.log(results);
+    } catch (err) {
+      console.log(err);
+    }
   },
-
-  selectRandomMessage: () => {
-    const text = 'SELECT * FROM messages order by random() LIMIT 1';
-    pool.query(text)
-      .then(results => console.log(results))
-      .catch(err => console.log(err));
+  
+  selectRandomMessage: async () => {
+    const text = 'SELECT * FROM messages ORDER BY random() LIMIT 1';
+    try {
+      const results = await pool.query(text);
+      return results.rows[0];
+    } catch (err) {
+      console.log(err);
+    }
   },
-
-  deleteAllMessages: () => {
+  
+  deleteAllMessages: async () => {
     const text = 'DELETE FROM messages';
-    pool.query(text)
-      .then(results => console.log(results))
-      .catch(err => console.log(err));
+    try {
+      const results = await pool.query(text);
+      console.log(results);
+    } catch (err) {
+      console.log(err);
+    }
   },
-
 }
